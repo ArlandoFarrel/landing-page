755af80aeec0d3ae7ac837d18229fd5bbcd1de8a
ArlandoFarrel <arlando.farrel@gmail.com>
2023-02-01 10:47:10 +0700

    Add Filtering for Categories

--- src/App.js
+++ src/App.js
@@ -1,73 +1,15 @@
-import data from "./data.json";
-import './style.css';
-import React, {useEffect, useState} from "react";
-
+import React, { useState } from 'react'
+import Card from './Components/Card';
+import './App.css';
 function App() {
-  // const url = 
-  // 'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/1f2898db-ceb4-4565-a1eb-2d3beb8a509c/product.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230130%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230130T061259Z&X-Amz-Expires=86400&X-Amz-Signature=e0539929a1be2b17856e3a32a94d174845e9577391b7d945900f901fd3117da3&X-Amz-SignedHeaders=host&x-id=GetObject'
-  // const [users, setUsers] = useState([]);
-  // const getDataUsers = async () => {
-  //   const response = await fetch('https://s3.us-west-2.amazonaws.com/secure.notion-static.com/1f2898db-ceb4-4565-a1eb-2d3beb8a509c/product.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230130%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230130T061259Z&X-Amz-Expires=86400&X-Amz-Signature=e0539929a1be2b17856e3a32a94d174845e9577391b7d945900f901fd3117da3&X-Amz-SignedHeaders=host&x-id=GetObject');
-  //   const dataku = await response.json();
-  //   const users = dataku.slice(0, 8);
-  //   setUsers(users)
-  // }
-  // useEffect(() => {
-  //   getDataUsers()
-  // })
-//   const [error, setError] = useState(null);
-//     const [isLoaded, setIsLoaded] = useState(false);
-//     const [items, setItems] = useState([]);
-//   useEffect(() => {
-//     fetch(
-//         "https://s3.us-west-2.amazonaws.com/secure.notion-static.com/1f2898db-ceb4-4565-a1eb-2d3beb8a509c/product.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230128%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230128T070926Z&X-Amz-Expires=86400&X-Amz-Signature=c9f59a346d3e44bd3068dfe2842648238c4f0287cd623f1e40018bb95bb8c085&X-Amz-SignedHeaders=host&x-id=GetObject"
-//     )
-//         .then((res) => res.json())
-//         .then(
-//             (result) => {
-//                 setIsLoaded(true);
-//                 setItems(result);
-//             },
-//             (error) => {
-//                 setIsLoaded(true);
-//                 setError(error);
-//             }
-//         );
-// }, []);
 
-  const [searchTerm, setSearchTerm] = useState(""); 
+
   return (
-    <div className="templateContainer">
-      <div className="searchInput_Container">
-        <input id="searchInput" type="text" placeholder="Search Here" onChange={(event) => {
-          setSearchTerm(event.target.value);
-        }} />
-      </div>
-      <div className="template_Container">
-        {
-          data
-            .filter((val) => {
-              if(searchTerm == ""){
-                return val;
-              } else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-                return val;
-              }
-            })
-            .map((val) => {
-              return(
-                <div className="template" key={val.id}>
-                  <img src={val.image} alt="" />
-                  <h3 class="card-name">{val.name}</h3>
-                  <p class="card-text">{val.description}</p>
-                  <p className="price">Rp {val.price}</p>
-
-                  </div>
-              )
-            })
-        }
-      </div>
+    <div>
+      {/* <FilterProduct filterValueSelected={onFilterValueSelected}></FilterProduct> */}
+      <Card></Card>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/Card.js
@@ -0,0 +1,94 @@
+import data from "../data.json";
+import './Card.css';
+import React, {  useEffect, useState } from "react";
+
+
+function Card() {
+  const [keyword, setKeyword] = useState('');
+  const [hasilFilter, setHasilfilter] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  // const [search, setSearch] = useState('');
+  const handlefilter = (e) => {
+    const filterData = data.filter((e) => {
+      return e.category.toLowerCase().includes(keyword.toLowerCase());
+    });
+    setHasilfilter(filterData);
+  };
+  console.log(keyword);
+
+  useEffect(()=>{
+    handlefilter();
+  },[data, keyword])
+
+  function getFiltered(filtered) {
+    if (filtered===0) {
+      return 'Barang Tidak Ada';
+    }else {
+      return filtered + ' Products found';
+    }
+  }
+
+  return (
+    <div className="templateContainer">
+      
+      <div className="searchInput_Container">
+        <input id="searchInput" type="text" placeholder="Search Here" onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }} />
+
+       <div className="filter-area">
+        <form onChange={handlefilter}>
+         <select className="filter" name="isAvailable" value={keyword} onChange={(e) => setKeyword(e.target.value)}>
+               <option value="">All</option>
+               <option value="Ready">Ready</option>
+               <option value="Pre-Order">Pre-Order</option>
+               <option value="Barang Bekas">Barang Bekas</option>
+           </select>
+           </form>
+        </div>
+        
+      </div>
+      
+      <div className="information">
+                {getFiltered(hasilFilter.length)}
+      </div>
+
+      <div className="template_Container">
+        {hasilFilter.length>0?
+        hasilFilter
+          .filter((products) => (searchTerm !== '' ? products.name.toLowerCase().includes(searchTerm) : products))
+          .map((item) => (
+            <div className="template" key={item.id}>
+                  <img src={item.image} alt="" />
+                  <h3 class="card-name">{item.name}</h3>
+                  <p class="card-text">{item.description}</p>
+                  <p className="price">Rp {item.price}</p>
+
+                </div>
+          )):
+          data
+            .filter((val) => {
+              if (searchTerm == "") {
+                return val;
+              } else if (val.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+                return val;
+              }
+            })
+            .map((val) => {
+              return (
+                <div className="template" key={val.id}>
+                  <img src={val.image} alt="" />
+                  <h3 class="card-name">{val.name}</h3>
+                  <p class="card-text">{val.description}</p>
+                  <p className="price">Rp {val.price}</p>
+
+                </div>
+              )
+            })
+          }
+      </div>
+    </div>
+  );
+}
+
+export default Card;
