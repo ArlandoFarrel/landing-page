3b74e20cdb078e53e4435f2b8d8c9a6d45a3f665
Fadhila Zalfa Alisya <101534228+zalfaa22@users.noreply.github.com>
2023-02-03 15:02:20 +0700

    add footer

---
+++ Footer.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {
+Box,
+Container,
+Row,
+Column,
+FooterLink,
+Heading,
+} from "./FooterStyles";
+
+const Footer = () => {
+return (
+	<Box>
+	<h1 style={{ color: "green",
+				textAlign: "center",
+				marginTop: "-50px" }}>
+		Group 3
+	</h1>
+	<Container>
+		<Row>
+		<Column>
+			<Heading>Our Team</Heading>
+			<FooterLink href="#">Arlando Muhammad</FooterLink>
+			<FooterLink href="#">Fadhila Zalfa</FooterLink>
+			<FooterLink href="#">Griselda Putri</FooterLink>
+            <FooterLink href="#">Khusnul Faddila</FooterLink>
+		</Column>
+        <Column>
+			<Heading>Hobby</Heading>
+			<FooterLink href="#">Coding</FooterLink>
+			<FooterLink href="#">Design</FooterLink>
+			<FooterLink href="#">Design</FooterLink>
+			<FooterLink href="#">Writing</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Our School</Heading>
+			<FooterLink href="#">SMK Telkom Malang</FooterLink>
+		</Column>
+		<Column>
+			<Heading>Social Media</Heading>
+			<FooterLink href="#">
+			<i className="fab fa-facebook-f">
+				<span style={{ marginLeft: "10px" }}>
+				faafarrel
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-instagram">
+				<span style={{ marginLeft: "10px" }}>
+				zalfalisya
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-twitter">
+				<span style={{ marginLeft: "10px" }}>
+				griseldaputric
+				</span>
+			</i>
+			</FooterLink>
+			<FooterLink href="#">
+			<i className="fab fa-youtube">
+				<span style={{ marginLeft: "10px" }}>
+				nlfaa
+				</span>
+			</i>
+			</FooterLink>
+		</Column>
+		</Row>
+	</Container>
+	</Box>
+);
+};
+export default Footer;
