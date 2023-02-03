fb18186ceec7ef473622d4b16402bb51e4faa3aa
Fadhila Zalfa Alisya <101534228+zalfaa22@users.noreply.github.com>
2023-02-03 15:06:03 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header'
 import Main from './Components/Main'
-import Footer from '././Components/Footer';
+import Footer from './Components/Footer';
 
 function App() {
   return (
